be348cc514c66c7e9eb22392a8d05c3ab5c3b0b9
Kiran Anand <liteninkiran@hotmail.com>
2020-11-04 22:36:21 +0000

    Adding content and styling

--- wp-content/themes/kiran/front-page.php
+++ wp-content/themes/kiran/front-page.php
@@ -1,20 +1,27 @@
 
 <?php get_header(); ?>
 
+    <div class="container pt-5 pb-5">
+
+        <h1><?php echo the_title(); ?></h1>
+
+    </div>
+
+<?php
+
+    if(have_posts())
+    {
+        while(have_posts())
+        {
+            the_post();
+            echo the_content();
+        }
+    }
+
+?>
+
     <div class="container">
-
-        <div class="row">
-
-            <div class="col">
-                Left Side
-            </div>
-
-            <div class="col">
-                Right Side
-            </div>
-
-        </div>
-
+        <?php include 'lorem.php'; ?>
     </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/kiran/header.php
+++ wp-content/themes/kiran/header.php
@@ -5,3 +5,7 @@
     <title>Kiran Anand</title>
 </head>
 <body <?php body_class(); ?>>
+
+<header class="sticky-top">
+    
+</header>
